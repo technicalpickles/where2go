99dbdecff5551708794270da992f56301d2c964f
joshua.nichols <joshua.nichols@8d9064d8-a833-0410-bc82-453897b809b3>
2007-06-24 19:00:21 +0000

    Added some routing magic. / goes to cities->list. city/:name, now shows the city expressed by name.

--- trunk/app/models/city.rb
+++ trunk/app/models/city.rb
@@ -2,4 +2,10 @@ class City < ActiveRecord::Base
   validates_presence_of :name
   has_many :spots
   acts_as_mappable
+
+  def nice_name
+    name = self.name
+    name.sub! ' ', '_'
+    name.downcase!
+  end
 end
